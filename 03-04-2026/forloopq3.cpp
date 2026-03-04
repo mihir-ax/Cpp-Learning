cebc280ccac6b685d48454955ab8d18e8edb0333
MiHiR SEN <ms.mihir.ms@gmail.com>
2026-03-04 23:25:25 +0530

    q3

---
+++ 03-04-2026/forloopq3.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  // Write a program to print the sum of the digits of a given number n
+  // Example : If n = 512 the output should be 5 + 1 + 2 = 8
+
+
+  
+}
