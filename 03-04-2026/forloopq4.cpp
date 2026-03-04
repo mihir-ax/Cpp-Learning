39168127e7ac5b8239ff3bedd79b5b31f26214fe
MiHiR SEN <ms.mihir.ms@gmail.com>
2026-03-04 23:46:32 +0530

    Update forloopq4.cpp

--- 03-04-2026/forloopq4.cpp
+++ 03-04-2026/forloopq4.cpp
@@ -7,18 +7,16 @@ int main()
   // (Example : If input is 9874 9874, the output should be 4789 4789).
 
   int n = 5842;
-int last_digit = 0;
-int reverse = 0;
+  int last_digit = 0;
+  int reverse = 0;
 
+  while (n != 0)
+  {
 
-
-  while ( n != 0 ){
-
-last_digit = n % 10;
-cout << last_digit;
-last_digit = 0;
-n /= 10;
-
+    last_digit = n % 10;
+    cout << last_digit;
+    last_digit = 0;
+    n /= 10;
   }
 
   // cout << "Total Sum : " << n << endl;
