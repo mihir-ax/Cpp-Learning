f64805be81094f363b210d52e10229984cac8bd0
MiHiR SEN <ms.mihir.ms@gmail.com>
2026-03-04 23:30:19 +0530

    Update forloopq3.cpp

--- 03-04-2026/forloopq3.cpp
+++ 03-04-2026/forloopq3.cpp
@@ -6,6 +6,17 @@ int main()
   // Write a program to print the sum of the digits of a given number n
   // Example : If n = 512 the output should be 5 + 1 + 2 = 8
 
+  int n = 5842;
+  int last_digit = 0;
+  int sum = 0;
 
-  
+  while (n != 0)
+  {
+    last_digit = n % 10;
+    sum = sum + last_digit;
+    last_digit = 0;
+    n /= 10;
+  }
+
+  cout << "Total Sum : " << sum << endl;
 }
