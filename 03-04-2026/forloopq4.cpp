c6e32d232e11efb7b7f9e125bc1491105fa5079c
MiHiR SEN <ms.mihir.ms@gmail.com>
2026-03-04 23:46:22 +0530

    q4

---
+++ 03-04-2026/forloopq4.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  // Write a program to print the reverse of a given number.
+  // (Example : If input is 9874 9874, the output should be 4789 4789).
+
+  int n = 5842;
+int last_digit = 0;
+int reverse = 0;
+
+
+
+  while ( n != 0 ){
+
+last_digit = n % 10;
+cout << last_digit;
+last_digit = 0;
+n /= 10;
+
+  }
+
+  // cout << "Total Sum : " << n << endl;
+}
