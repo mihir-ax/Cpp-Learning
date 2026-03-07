0ef37813ade479b6f4719c45306eb44110cfb47a
MiHiR SEN <ms.mihir.ms@gmail.com>
2026-03-08 01:02:18 +0530

    hollow diamond

--- 03-07-2026/03_hollow_diamond.cpp
+++ 03-07-2026/03_hollow_diamond.cpp
@@ -1,19 +1,82 @@
 #include <iostream>
 using namespace std;
 
-
-void upperDiamond(int n){
-
-  for (int i = 1; i <= n; i++){
-    for (int j = 1; j <= n*2; j++){
-      
+void upperDiamond(int n)
+{
+  for (int i = 1; i <= n; i++)
+  {
+    for (int j = 1; j <= n * 2; j++)
+    {
+      if (i == 1)
+      {
+        if (j < n || j > n)
+        {
+          cout << "  ";
+        }
+        else
+        {
+          cout << "* ";
+        }
+      }
+      else
+      {
+        if (j <= n - i)
+        {
+          cout << "  ";
+        }
+        else if (j == n - i + 1)
+        {
+          cout << "* ";
+        }
+        else if ((j >= n - i + 1) && (j <= n + i - 2))
+        {
+          cout << "  ";
+        }
+        else if (j == n + i - 1)
+        {
+          cout << "* ";
+        }
+      }
     }
+    if (i != n)
+    {
+    }
+    cout << "\n";
   }
 }
 
-int main() {
+void lowerDiamond(int n)
+{
+  for (int i = n - 1; i >= 1; i--)
+  {
+    for (int j = 1; j <= n * 2; j++)
+    {
+      if ((j <= n - i) && (!(j > n - i)))
+      {
+        cout << "  ";
+      }
+      else if (j == n - i + 1)
+      {
+        cout << "* ";
+      }
+      else if ((j > n - i + 1) && (j < n + i - 1))
+      {
+        cout << "  ";
+      }
+      else if (j == n + i - 1)
+      {
+        cout << "* ";
+      }
+    }
+    cout << "\n";
+  }
+}
+
+int main()
+{
   int n;
   cout << "Enter the N : ";
   cin >> n;
   upperDiamond(n);
+  lowerDiamond(n);
 }
