4cd8e321e1d34e0ccca3534362b2f26d7efd951b
MiHiR SEN <ms.mihir.ms@gmail.com>
2026-03-15 15:46:01 +0530

    comeback !

--- 04_Pointers/01.cpp
+++ 04_Pointers/01.cpp
@@ -3,9 +3,16 @@ using namespace std;
 
 int main()
 {
-  int x = 50;
-  int *y = &x;
-  
-  cout << &x << endl;
-  cout << y;
+  // int x = 50;
+  // int *y = &x;
+
+  // cout << &x << endl;
+  // cout << y;
+
+// #comeback after 7 days !!
+
+  int xxy = 78;
+  int *p = &xxy;
+  cout << p;
+
 }
--- 04_Pointers/02_sum_of_2_number_POINTERS.cpp
+++ 04_Pointers/02_sum_of_2_number_POINTERS.cpp
@@ -4,15 +4,26 @@ using namespace std;
 int main()
 {
 
-  int a, b;
-  int *x = &a;
-  int *y = &b;
+  // int a, b;
+  // int *x = &a;
+  // int *y = &b;
 
-  cout << "ENter a number : " << endl;
-  cin >> *x;
-  cout << "ENter another number : " << endl;
-  cin >> *y;
+  // cout << "ENter a number : " << endl;
+  // cin >> *x;
+  // cout << "ENter another number : " << endl;
+  // cin >> *y;
 
-  cout << a + b << endl;
-  cout << *x + *y;
+  // cout << a + b << endl;
+  // cout << *x + *y;
+
+  int x, y;
+  cout << "Enter the Fist Number : ";
+  cin >> x;
+  cout << "Enter the Second Number : ";
+  cin >> y;
+
+  int *xx = &x;
+  int *yy = &y;
+
+  cout << "Sum of those two numbers is : " << *xx + *yy;
 }
