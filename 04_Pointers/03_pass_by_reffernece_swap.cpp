24986d56b567725d9c096402c3c10e6147e99fec
MiHiR SEN <ms.mihir.ms@gmail.com>
2026-03-15 16:20:48 +0530

    Create 03_pass_by_reffernece_swap.cpp

---
+++ 04_Pointers/03_pass_by_reffernece_swap.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+// void swap(int *x, int *y)
+// {
+//   int temp = *x;
+//   *x = *y;
+//   *y = temp;
+// }
+
+// int main()
+// {
+//   int num1 = 5;
+//   int num2 = 8;
+
+//   swap(&num1, &num2);
+
+//   cout << num1 << " " << num2;
+// }
+
+
+// one more way seedha adress hi badal do dono ka !!
+void swaap(int &a, int &b)
+{
+  int temp = a;
+  a = b;
+  b = temp;
+}
+
+int main()
+{
+  int a = 5, b = 7;
+
+  swaap(a, b);
+  cout << a << " " << b;
+}
