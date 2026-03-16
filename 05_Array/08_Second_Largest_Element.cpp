e84d51158de59b0dae36d2edd3708bd6b874c073
MiHiR SEN <ms.mihir.ms@gmail.com>
2026-03-16 15:21:08 +0530

    Create 08_Second_Largest_Element.cpp

---
+++ 05_Array/08_Second_Largest_Element.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  // Find the second largest element in the given array.
+
+  int arr[] = {5, 7, 8, 9, 11, 2, 15};
+  int largest = arr[0];
+  int sec_largest = arr[0];
+
+  for (int i = 0; i < (sizeof(arr) / sizeof(arr[0])); i++)
+  {
+    if (arr[i] > largest)
+    {
+      sec_largest = largest;
+      largest = arr[i];
+    }
+  }
+
+  cout << "Second largest number is : " << sec_largest;
+}
