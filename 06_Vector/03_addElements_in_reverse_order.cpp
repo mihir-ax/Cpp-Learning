badde973e7c6e27032b760e9bb95f15a055bd819
MiHiR SEN <ms.mihir.ms@gmail.com>
2026-03-20 00:31:51 +0530

    Update 03_addElements_in_reverse_order.cpp

--- 06_Vector/03_addElements_in_reverse_order.cpp
+++ 06_Vector/03_addElements_in_reverse_order.cpp
@@ -3,14 +3,22 @@ using namespace std;
 
 int main()
 {
-  vector<int> v = {1, 2, 3, 4, 5, 6, 7};
-  vector<int> v2;
+  vector<int> v = {3, 4, 7, 8, 10};
+  vector<int> v2(v.size());
 
-  for (int i = v.size() - 1; i >= 0; i--)
+  // for (int i = v.size() - 1; i >= 0; i--)
+  // {
+  //   for (int j = v.size() - i - 1; j <= v.size(); j++)
+  //   {
+  //     v2[j] = (v.at(i));
+  //     break;
+  //   }
+  // }
+
+  for (int i = 0; i <= v.size() - 1; i++)
   {
-    v2.push_back(v.at(i));
+    v2[i] = v[v.size() - 1 - i];
   }
-
   for (int i = 0; i < v2.size(); i++)
   {
     cout << v2[i] << " ";
