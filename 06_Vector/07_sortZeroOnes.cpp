ae08c2e2273e7e029ef544ec28860a8c00982107
MiHiR SEN <ms.mihir.ms@gmail.com>
2026-03-21 15:28:19 +0530

    Create 07_sortZeroOnes.cpp

---
+++ 06_Vector/07_sortZeroOnes.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  vector<int> v = {1, 1, 1, 1, 1, 0, 0};
+
+  // ---------------
+  //   Method - 1
+  // ---------------
+  // int noz = 0;
+  // int noo = 0;
+  // for (int i = 0; i < v.size(); i++)
+  // {
+  //   if (v[i] == 0)
+  //     noz++;
+  //   else
+  //     noo++;
+  // }
+  // for (int i = 0; i < v.size(); i++)
+  // {
+  //   if (i <= noz - 1)
+  //     v[i] = 0;
+  //   else
+  //     v[i] = 1;
+  // }
+
+  // for (int i = 0; i < v.size(); i++)
+  // {
+  //   cout << v[i] << " ";
+  // }
+
+  // ---------------
+  //   Method - 2
+  // ---------------
+  int i = 0;
+  int j = v.size() - 1;
+  while (i < j)
+  {
+    if (v[i] == 0)
+      i++;
+    if (v[j] == 1)
+      j--;
+    if ((v[i] == 1) && (v[j] == 0))
+    {
+      int *f = &v[i];
+      int *l = &v[j];
+      int temp = *f;
+      *f = *l;
+      *l = temp;
+    }
+  }
+  for (int i = 0; i < v.size(); i++)
+  {
+    cout << v[i] << " ";
+  }
+}
